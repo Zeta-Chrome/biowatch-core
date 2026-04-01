de39dfacad2b6e34d64b2328e8b18daadc8bd0c4
Zeta-Chrome <abel.punnoose.cosmic@gmail.com>
2026-04-01 12:42:31 +0530

    Add uart hal

---
+++ hal/uart/uart.c
@@ -0,0 +1,51 @@
+#include "hal/uart/uart.h"
+#include "hal/gpio/gpio.h"
+#include "hal/reg.h"
+#include "stm32wb55xx.h"
+
+/*00: PCLK selected as USART1 clock
+ *01: System clock (SYSCLK) selected as USART1 clock
+ *10: HSI16 clock selected as USART1 clock
+ *11: LSE clock selected as USART1 clock 
+*/
+
+void hal_uart_init(uart_conf_t *conf)
+{
+    gpio_conf_t gpio_conf = gpio_conf_af(conf->tx, conf->af, GPIO_TYPE_PUSH_PULL, GPIO_PULL_NONE);
+    hal_gpio_init(&gpio_conf);
+
+    // enable clock
+    reg_set_bit(&RCC->APB2ENR, RCC_APB2ENR_USART1EN_Pos); 
+    reg_set_field(&RCC->CCIPR, RCC_CCIPR_USART1SEL_Pos, 2, 0);
+
+    // Set the M bit (8 bits)
+    reg_clear_bit(&conf->uart->CR1, USART_CR1_M0_Pos);
+    reg_clear_bit(&conf->uart->CR1, USART_CR1_M1_Pos);
+
+    // Set the baud rate 
+    reg_clear_bit(&conf->uart->CR1, USART_CR1_OVER8_Pos);
+    reg_set_field(&conf->uart->BRR, 0, 16, USART_KER_CK / (USART_PRESC + 1) / conf->baud_rate);
+
+    // Configure stop bits (1 bit)
+    reg_set_field(&conf->uart->CR2, USART_CR2_STOP_Pos, 2, 0);
+
+    // Enable USART 
+    reg_set_bit(&conf->uart->CR1, USART_CR1_UE_Pos);
+
+    // Send Idle frame
+    reg_set_bit(&conf->uart->CR1, USART_CR1_TE_Pos);
+}
+
+void hal_uart_write(USART_TypeDef *uart, uint8_t* data, uint16_t len)
+{
+    while(len > 0)
+    {
+        // Wait till TXE = 1 to send next byte
+        while(!(uart->ISR & USART_ISR_TXE));
+        uart->TDR = *data++;
+        len--;
+    }
+    
+    // Wait till TC = 1
+    while(!(uart->ISR & USART_ISR_TC));
+}
---
+++ hal/uart/uart.h
@@ -0,0 +1,31 @@
+#ifndef HAL_UART_H
+#define HAL_UART_H
+
+#include "hal/gpio/gpio_types.h"
+#include "stm32wb55xx.h"
+#include <stdint.h>
+
+#define USART_KER_CK SystemCoreClock 
+#define USART_PRESC 0
+
+typedef enum
+{
+    UART_BAUD_9600 = 9600,
+    UART_BAUD_19200 = 19200,
+    UART_BAUD_38400 = 38400,
+    UART_BAUD_57600 = 57600,
+    UART_BAUD_115200 = 115200,
+} uart_baud_t;
+
+typedef struct
+{
+    gpio_t tx;
+    gpio_af_t af;
+    USART_TypeDef *uart;
+    uart_baud_t baud_rate;
+} uart_conf_t;
+
+void hal_uart_init(uart_conf_t *conf);
+void hal_uart_write(USART_TypeDef *uart, uint8_t* data, uint16_t len);
+
+#endif
