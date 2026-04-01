e05c0f22326ec1cfe72569a1ad4b6c239982298b
Zeta-Chrome <abel.punnoose.cosmic@gmail.com>
2026-04-01 12:46:41 +0530

    Add i2c hal

---
+++ hal/i2c/i2c.c
@@ -0,0 +1,284 @@
+#include "hal/i2c/i2c.h"
+#include "hal/gpio/gpio.h"
+#include "hal/gpio/gpio_types.h"
+#include "hal/reg.h"
+#include "utils/status.h"
+#include "utils/assert.h"
+#include "stm32wb55xx.h"
+#include <stdint.h>
+
+static i2c_handle_t *g_i2c_handles[MAX_I2C_PERIPHERALS];
+
+static void i2c_clock_init(I2C_TypeDef *i2c)
+{
+    if (i2c == I2C1)
+    {
+        reg_set_field(&RCC->CCIPR, RCC_CCIPR_I2C1SEL_Pos, 2, 0x1);  // System Clock
+        reg_set_bit(&RCC->APB1ENR1, RCC_APB1ENR1_I2C1EN_Pos);
+    }
+    else if (i2c == I2C3)
+    {
+        reg_set_field(&RCC->CCIPR, RCC_CCIPR_I2C3SEL_Pos, 2, 0x1);  // System Clock
+        reg_set_bit(&RCC->APB1ENR1, RCC_APB1ENR1_I2C3EN_Pos);
+    }
+}
+
+static void i2c_gpio_init(i2c_conf_t *conf)
+{
+    gpio_conf_t gpio_conf = gpio_conf_af(conf->sda, conf->af, GPIO_TYPE_OPEN_DRAIN, GPIO_PULL_UP);
+    hal_gpio_init(&gpio_conf);
+
+    gpio_conf.gpio = conf->scl;
+    hal_gpio_init(&gpio_conf);
+}
+
+void hal_i2c_init(i2c_conf_t *conf)
+{
+    BW_ASSERT(conf->dnf < 16, "Invalid DNF: %d (Expected 0-15)", conf->dnf);
+
+    // Configure I2C clock
+    i2c_clock_init(conf->i2c);
+
+    // SDA and SCL gpio init
+    i2c_gpio_init(conf);
+
+    // Clear the PE bit
+    reg_clear_bit(&conf->i2c->CR1, I2C_CR1_PE_Pos);
+
+    // Configure Analog and digital filter
+    reg_set_field(&conf->i2c->CR1, I2C_CR1_DNF_Pos, 4, conf->dnf);
+
+    // Configure the timing
+    switch (conf->speed)
+    {
+    case I2C_SPEED_STANDARD:
+    {
+        reg_set_field(&conf->i2c->TIMINGR, I2C_TIMINGR_PRESC_Pos, 4, 0x3);
+        reg_set_field(&conf->i2c->TIMINGR, I2C_TIMINGR_SCLL_Pos, 8, 0x13);
+        reg_set_field(&conf->i2c->TIMINGR, I2C_TIMINGR_SCLH_Pos, 8, 0xF);
+        reg_set_field(&conf->i2c->TIMINGR, I2C_TIMINGR_SDADEL_Pos, 4, 0x2);
+        reg_set_field(&conf->i2c->TIMINGR, I2C_TIMINGR_SCLDEL_Pos, 4, 0x4);
+        break;
+    }
+    case I2C_SPEED_FAST:
+    {
+        reg_set_field(&conf->i2c->TIMINGR, I2C_TIMINGR_PRESC_Pos, 4, 0x1);
+        reg_set_field(&conf->i2c->TIMINGR, I2C_TIMINGR_SCLL_Pos, 8, 0x9);
+        reg_set_field(&conf->i2c->TIMINGR, I2C_TIMINGR_SCLH_Pos, 8, 0x3);
+        reg_set_field(&conf->i2c->TIMINGR, I2C_TIMINGR_SDADEL_Pos, 4, 0x2);
+        reg_set_field(&conf->i2c->TIMINGR, I2C_TIMINGR_SCLDEL_Pos, 4, 0x3);
+        break;
+    }
+    case I2C_SPEED_FAST_PLUS:
+    {
+        reg_set_field(&conf->i2c->TIMINGR, I2C_TIMINGR_PRESC_Pos, 4, 0x0);
+        reg_set_field(&conf->i2c->TIMINGR, I2C_TIMINGR_SCLL_Pos, 8, 0x4);
+        reg_set_field(&conf->i2c->TIMINGR, I2C_TIMINGR_SCLH_Pos, 8, 0x2);
+        reg_set_field(&conf->i2c->TIMINGR, I2C_TIMINGR_SDADEL_Pos, 4, 0x0);
+        reg_set_field(&conf->i2c->TIMINGR, I2C_TIMINGR_SCLDEL_Pos, 4, 0x2);
+        break;
+    }
+    }
+
+    // Configure NOSTRETCH
+    reg_clear_bit(&conf->i2c->CR1, I2C_CR1_NOSTRETCH_Pos);
+
+    // Set PE bit
+    reg_set_bit(&conf->i2c->CR1, I2C_CR1_PE_Pos);
+}
+
+void hal_i2c_reset(I2C_TypeDef *i2c)
+{
+    // 3 step process to reset
+    reg_clear_bit(&i2c->CR1, I2C_CR1_PE_Pos);
+    if (!reg_get_bit(&i2c->CR1, I2C_CR1_PE_Pos))
+    {
+        reg_set_bit(&i2c->CR1, I2C_CR1_PE_Pos);
+    }
+}
+
+static void i2c_configure_reload(i2c_handle_t *handle)
+{
+    if (handle->len > 255)
+    {
+        reg_set_field(&handle->i2c->CR2, I2C_CR2_NBYTES_Pos, 8, 255);
+        reg_set_bit(&handle->i2c->CR2, I2C_CR2_RELOAD_Pos);
+        reg_clear_bit(&handle->i2c->CR2, I2C_CR2_AUTOEND_Pos);
+    }
+    else
+    {
+        reg_set_field(&handle->i2c->CR2, I2C_CR2_NBYTES_Pos, 8, handle->len);
+        reg_clear_bit(&handle->i2c->CR2, I2C_CR2_RELOAD_Pos);
+        if (handle->repeat)
+        {
+            reg_clear_bit(&handle->i2c->CR2, I2C_CR2_AUTOEND_Pos);
+        }
+        else
+        {
+            reg_set_bit(&handle->i2c->CR2, I2C_CR2_AUTOEND_Pos);
+        }
+    }
+}
+
+void hal_i2c_receive(i2c_handle_t *handle)
+{
+    handle->type = I2C_TYPE_RX;
+    handle->total = handle->len;
+    if (handle->len == 0)
+    {
+        return;
+    }
+
+    // Store the handles
+    if (handle->i2c == I2C1)
+    {
+        g_i2c_handles[I2C_PERIPH_1] = handle;
+    }
+    else if (handle->i2c == I2C3)
+    {
+        g_i2c_handles[I2C_PERIPH_3] = handle;
+    }
+
+    // Clear old fields
+    handle->i2c->CR2 = 0;
+
+    // Set 7 bit address
+    reg_clear_bit(&handle->i2c->CR2, I2C_CR2_ADD10_Pos);                      // 0 = 7 bit address
+    reg_set_field(&handle->i2c->CR2, I2C_CR2_SADD_Pos + 1, 7, handle->addr);  // left shift by one
+
+    // Set RXIE for interrupt
+    reg_set_mask(&handle->i2c->CR1, I2C_CR1_RXIE_Msk | I2C_CR1_TCIE_Msk | I2C_CR1_NACKIE_Msk |
+                                    I2C_CR1_STOPIE_Msk | I2C_CR1_ERRIE_Msk);
+
+    // Set transfer direction (1 = read)
+    reg_set_bit(&handle->i2c->CR2, I2C_CR2_RD_WRN_Pos);
+
+    // Configure NBYTES, RELOAD and AUTOEND
+    i2c_configure_reload(handle);
+
+    // Set start bit
+    reg_set_bit(&handle->i2c->CR2, I2C_CR2_START_Pos);
+}
+
+void hal_i2c_transmit(i2c_handle_t *handle)
+{
+    handle->type = I2C_TYPE_TX;
+    handle->total = handle->len;
+    if (handle->len == 0)
+    {
+        return;
+    }
+
+    // Store the handles
+    if (handle->i2c == I2C1)
+    {
+        g_i2c_handles[I2C_PERIPH_1] = handle;
+    }
+    else if (handle->i2c == I2C3)
+    {
+        g_i2c_handles[I2C_PERIPH_3] = handle;
+    }
+
+    // Clear old fields
+    handle->i2c->CR2 = 0;
+
+    // Set 7 bit address
+    reg_clear_bit(&handle->i2c->CR2, I2C_CR2_ADD10_Pos);                      // 0 = 7 bit address
+    reg_set_field(&handle->i2c->CR2, I2C_CR2_SADD_Pos + 1, 7, handle->addr);  // left shift by one
+
+    // Set TXIE for interrupt
+    reg_set_mask(&handle->i2c->CR1, I2C_CR1_TXIE_Msk | I2C_CR1_TCIE_Msk | I2C_CR1_NACKIE_Msk |
+                                    I2C_CR1_STOPIE_Msk | I2C_CR1_ERRIE_Msk);
+
+    // Set transfer direction (0 = write)
+    reg_clear_bit(&handle->i2c->CR2, I2C_CR2_RD_WRN_Pos);
+
+    // Configure NBYTES, RELOAD and AUTOEND
+    i2c_configure_reload(handle);
+
+    // Set start bit
+    reg_set_bit(&handle->i2c->CR2, I2C_CR2_START_Pos);
+}
+
+void hal_i2c_ev_isr(i2c_perip_t type)
+{
+    i2c_handle_t *handle = g_i2c_handles[type];
+    I2C_TypeDef *i2c = handle->i2c;
+    
+    if (reg_get_bit(&i2c->ISR, I2C_ISR_STOPF_Pos) || reg_get_bit(&i2c->ISR, I2C_ISR_NACKF_Pos))
+    {
+        // Clear all interrupt bits
+        reg_clear_mask(&handle->i2c->CR1, I2C_CR1_RXIE_Msk | I2C_CR1_TXIE_Msk | I2C_CR1_TCIE_Msk |
+                                          I2C_CR1_NACKIE_Msk | I2C_CR1_STOPIE_Msk | I2C_CR1_ERRIE_Msk);
+    }
+
+    if (reg_get_bit(&i2c->ISR, I2C_ISR_NACKF_Pos))
+    {
+        reg_set_bit(&i2c->ICR, I2C_ICR_NACKCF_Pos);
+        handle->on_complete(STATUS_I2C_NACKF);
+        return;
+    }
+
+    if (reg_get_bit(&i2c->ISR, I2C_ISR_STOPF_Pos))
+    {
+        reg_set_bit(&i2c->ICR, I2C_ICR_STOPCF_Pos);
+        handle->on_complete(STATUS_OK);
+        return;
+    }
+
+    if (reg_get_bit(&i2c->ISR, I2C_ISR_RXNE_Pos))
+    {
+        uint16_t idx = handle->total - handle->len--;
+        *(handle->buf + idx) = i2c->RXDR;
+        return;
+    }
+
+    if (reg_get_bit(&i2c->ISR, I2C_ISR_TXIS_Pos))
+    {
+        uint16_t idx = handle->total - handle->len--;
+        i2c->TXDR = *(handle->buf + idx);
+        return;
+    }
+
+    if (reg_get_bit(&i2c->ISR, I2C_ISR_TC_Pos))  // only happens during repeated start
+    {
+        // clear trasmission and receiver interrupts
+        reg_clear_bit(&handle->i2c->CR1, I2C_CR1_TXIE_Pos);
+        reg_clear_bit(&handle->i2c->CR1, I2C_CR1_RXIE_Pos); 
+
+        // callback to continue the repeated start
+        handle->on_complete(STATUS_I2C_REPEATED_START);
+
+        // Set transfer direction (1 = read)
+        reg_set_field(&handle->i2c->CR2, I2C_CR2_RD_WRN_Pos, 1, handle->type);
+
+        // Configure NBYTES, RELOAD and AUTOEND
+        i2c_configure_reload(handle);
+
+        // Set start bit
+        reg_set_bit(&handle->i2c->CR2, I2C_CR2_START_Pos);
+        return;
+    }
+
+    if (reg_get_bit(&i2c->ISR, I2C_ISR_TCR_Pos))
+    {
+        i2c_configure_reload(handle);
+        return;
+    }
+}
+
+void hal_i2c_er_isr(i2c_perip_t type)
+{
+    i2c_handle_t *handle = g_i2c_handles[type];
+
+    reg_clear_mask(&handle->i2c->CR1, I2C_CR1_RXIE_Msk | I2C_CR1_TXIE_Msk | I2C_CR1_TCIE_Msk |
+                                      I2C_CR1_NACKIE_Msk | I2C_CR1_STOPIE_Msk | I2C_CR1_ERRIE_Msk);
+
+    handle->on_complete(STATUS_I2C_ERR);
+}
+
+void hal_i2c_deinit(I2C_TypeDef *i2c)
+{
+    reg_clear_mask(&i2c->CR1, I2C_CR1_RXIE_Msk | I2C_CR1_TXIE_Msk | I2C_CR1_TCIE_Msk |
+                              I2C_CR1_NACKIE_Msk | I2C_CR1_STOPIE_Msk | I2C_CR1_ERRIE_Msk);
+}
---
+++ hal/i2c/i2c.h
@@ -0,0 +1,63 @@
+#ifndef HAL_I2C_H
+#define HAL_I2C_H
+
+#include "hal/gpio/gpio_types.h"
+#include "utils/status.h"
+#include "stm32wb55xx.h"
+#include <stdbool.h>
+
+typedef enum
+{
+    I2C_TYPE_TX,
+    I2C_TYPE_RX,
+} i2c_t;
+
+typedef enum
+{
+    I2C_PERIPH_1,
+    I2C_PERIPH_3,
+    MAX_I2C_PERIPHERALS
+} i2c_perip_t;
+
+typedef enum
+{
+    I2C_SPEED_STANDARD,
+    I2C_SPEED_FAST,
+    I2C_SPEED_FAST_PLUS
+} i2c_speed_t;
+
+typedef struct
+{
+    gpio_t sda;
+    gpio_t scl;
+    gpio_af_t af;
+    I2C_TypeDef *i2c;
+    i2c_speed_t speed;
+    uint8_t dnf;  // 0->15
+} i2c_conf_t;
+
+typedef struct
+{
+    i2c_t type;
+    I2C_TypeDef *i2c;
+    uint8_t addr;
+
+    uint8_t *buf;
+    uint16_t len;
+    uint16_t total;
+
+    bool repeat;
+
+    void (*on_complete)(bw_status_t);
+} i2c_handle_t;
+
+void hal_i2c_init(i2c_conf_t *conf);
+void hal_i2c_reset(I2C_TypeDef *i2c);
+void hal_i2c_ev_isr(i2c_perip_t type);
+void hal_i2c_er_isr(i2c_perip_t type);
+void hal_i2c_receive(i2c_handle_t *i2c_h);
+void hal_i2c_transmit(i2c_handle_t *i2c_h);
+void hal_i2c_transmit_receive(i2c_handle_t *i2c_h);
+void hal_i2c_deinit(I2C_TypeDef *i2c);
+
+#endif
