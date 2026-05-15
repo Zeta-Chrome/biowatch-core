50191d29244f00c4ae250d57ba5d8bec90c155b7
Zeta-Chrome <abel.punnoose.cosmic@gmail.com>
2026-05-15 13:13:32 +0530

    Add Idle Hook

--- rtos/rtos.c
+++ rtos/rtos.c
@@ -6,12 +6,19 @@
 #include "task/task.h"
 #include "utils.h"
 
+static rtos_idle_hook_t g_idle_hook;
+
 void rtos_scheduler_tick();
 
-static void sleep_task(void *user_data)
+static void idle_task(void *user_data)
 {
     (void)user_data;
-    while (1);
+    while (1)
+    {
+        RTOS_ENTER_CRITICAL();
+        g_idle_hook(user_data);
+        RTOS_EXIT_CRITICAL();
+    }
 }
 
 void rtos_init(rtos_conf_t *conf)
@@ -25,8 +32,11 @@ void rtos_init(rtos_conf_t *conf)
         BW_LOG("Exhausted task stack space of %zu", TASK_POOL_SIZE);
         bw_error_handler();
     }
+
+    g_idle_hook = conf->idle_hook;
+
     rtos_task_init();
-    rtos_task_create(sleep_task, "_SLEEP_TASK", MAX_TASK_PRIORITY, 32, NULL, NULL);
+    rtos_task_create(idle_task, "_SLEEP_TASK", MAX_TASK_PRIORITY, 32, conf->idle_data, NULL);
 
     lptim_conf_t lptim_conf = {.priority = HIGHEST_IRQ_PRIO, .callback = rtos_scheduler_tick, NULL};
     hal_lptim_init(&lptim_conf);
--- rtos/rtos.h
+++ rtos/rtos.h
@@ -8,9 +8,13 @@
 #include "rtos/sync/mutex.h"
 #include "rtos/sync/event.h"
 
+typedef void (*rtos_idle_hook_t)(void *);
+
 typedef struct 
 {
     rtos_pool_conf_t pool_confs[MEM_BLOCK_COUNT];
+    rtos_idle_hook_t idle_hook;
+    void *idle_data;
 } rtos_conf_t;
 
 void rtos_init(rtos_conf_t *conf);
