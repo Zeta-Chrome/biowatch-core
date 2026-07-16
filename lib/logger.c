efdd38fa98bb9c5e004ec359a31e70a1c602592b
Zeta-Chrome <abel.punnoose.cosmic@gmail.com>
2026-07-16 14:37:00 +0530

    Add basic floating point numbers printing

--- lib/logger.c
+++ lib/logger.c
@@ -1,5 +1,6 @@
 #ifdef DEBUG
 
+#include "kernel/critical.h"
 #include "logger.h"
 #include <stdarg.h>
 #include <stdint.h>
@@ -24,6 +25,18 @@ static const gpio_t PL_UART_TX = {.port = GPIOA, .pin = 9}; // PA9
         }                      \
     } while (0)
 
+typedef union
+{
+    double f;
+    uint64_t raw;
+    struct
+    {
+        uint64_t mantissa : 52;
+        uint64_t exponent : 11;
+        uint64_t sign : 1;
+    } bits;
+} ieee754_double_t;
+
 static char g_msg[128];
 
 void bw_logger_init()
@@ -36,6 +49,15 @@ void bw_logger_init()
     SEGGER_RTT_Init();
     SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
 #endif
+}
+
+static inline void put_str(char **msg, int *msg_len, const char *s)
+{
+    while (*s)
+    {
+        PUTC(*msg, *msg_len, *s);
+        s++;
+    }
 }
 
 static inline void put_digits(char **msg, int *msg_len, uint32_t d, int base)
@@ -61,6 +83,80 @@ static inline void put_digits(char **msg, int *msg_len, uint32_t d, int base)
     }
 }
 
+static inline void put_float(char **msg, int *msg_len, ieee754_double_t *df)
+{
+    uint16_t raw_exp = df->bits.exponent; /* 0..2047, no bias subtracted */
+    uint64_t mantissa = df->bits.mantissa;
+    uint8_t sign = df->bits.sign;
+
+    if (raw_exp == 0x7FF) /* inf / nan */
+    {
+        if (mantissa == 0)
+        {
+            put_str(msg, msg_len, sign ? "-inf" : "inf");
+        }
+        else
+        {
+            put_str(msg, msg_len, "nan");
+        }
+        return;
+    }
+
+    if (sign)
+    {
+        PUTC(*msg, *msg_len, '-');
+    }
+
+    double f = df->f;
+    if (f < 0)
+        f = -f;
+
+    uint32_t int_part = (uint32_t)f;
+    double frac = f - (double)int_part;
+
+    put_digits(msg, msg_len, int_part, 10);
+
+    /* how many digits did the integer part use? */
+    uint32_t tmp = int_part;
+    int int_digits = (tmp == 0) ? 1 : 0;
+    while (tmp > 0)
+    {
+        int_digits++;
+        tmp /= 10;
+    }
+
+    int max_frac = 5;
+    int budget = 10 - int_digits;
+    if (budget < max_frac)
+        max_frac = budget;
+    if (max_frac < 0)
+        max_frac = 0;
+
+    char digits[5];
+    int n = 0;
+    for (int i = 0; i < max_frac; i++)
+    {
+        frac *= 10;
+        uint32_t d = (uint32_t)frac;
+        digits[n++] = (char)('0' + d);
+        frac -= d;
+    }
+
+    while (n > 0 && digits[n - 1] == '0') // trim trailing zeros
+    {
+        n--;
+    }
+
+    if (n > 0)
+    {
+        PUTC(*msg, *msg_len, '.');
+        for (int i = 0; i < n; i++)
+        {
+            PUTC(*msg, *msg_len, digits[i]);
+        }
+    }
+}
+
 static int str_formatv(char *msg, int msg_len, const char *fmt, va_list args)
 {
     int len = msg_len;
@@ -79,11 +175,7 @@ static int str_formatv(char *msg, int msg_len, const char *fmt, va_list args)
         case 's':
         {
             const char *s = va_arg(args, const char *);
-            while (*s)
-            {
-                PUTC(msg, msg_len, *s);
-                s++;
-            }
+            put_str(&msg, &msg_len, s);
             break;
         }
         case 'd':
@@ -100,6 +192,12 @@ static int str_formatv(char *msg, int msg_len, const char *fmt, va_list args)
         {
             int u = va_arg(args, uint32_t);
             put_digits(&msg, &msg_len, u, 10);
+            break;
+        }
+        case 'f':
+        {
+            double df = va_arg(args, double);
+            put_float(&msg, &msg_len, (ieee754_double_t *)&df);
             break;
         }
         case 'x':
@@ -174,26 +272,34 @@ void bw_print_s(const char *msg, int msg_len)
 
 void bw_log(const char *file, int line, const char *fmt, ...)
 {
+    KERNEL_ENTER_CRITICAL();
+
     const char *filename = strrchr(file, '/');
     filename = (filename) ? filename + 1 : file;
     int msg_len = str_format(g_msg, sizeof(g_msg), "[%s:%d]", filename, line);
 
     va_list args;
     va_start(args, fmt);
-    msg_len += str_formatv(g_msg + msg_len, sizeof(g_msg) - msg_len, fmt, args);
+    msg_len += str_formatv(g_msg + msg_len, (int)sizeof(g_msg) - msg_len, fmt, args);
     va_end(args);
 
     bw_print_s(g_msg, msg_len);
+
+    KERNEL_EXIT_CRITICAL();
 }
 
 void bw_print(const char *fmt, ...)
 {
+    KERNEL_ENTER_CRITICAL();
+
     va_list args;
     va_start(args, fmt);
     int msg_len = str_formatv(g_msg, sizeof(g_msg), fmt, args);
     va_end(args);
 
     bw_print_s(g_msg, msg_len);
+
+    KERNEL_EXIT_CRITICAL();
 }
 
 #endif
